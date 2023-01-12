6fa0e6a428ecee172c54bd51c25adcedea0d30c5
bit4bit <bit4bit@riseup.net>
2023-01-12 13:50:43 +0000

    fix: add missed
    FossilOrigin-Name: 66001a1ad4104ddee74092c7f10254c4489e0c512c7b54e81cebe919867b7c83

---
+++ src/gateway/event.cr
@@ -0,0 +1,31 @@
+require "json"
+
+module Bobo::Gateway
+  class Event
+    getter :tag
+    getter :event
+
+    private def initialize(@tag : String, @event : Bobo::Application::Events::Event)
+    end
+
+    def to_wire : String
+      {"tag" => @tag,
+       "event" => @event.to_wire}.to_json
+    end
+
+    def self.create(event : Bobo::Application::Events::ResourceDrived)
+      new("resource-drived", event)
+    end
+    def self.from_wire(wire : String) : self
+      data = Hash(String, String).from_json(wire)
+      tag = data["tag"].not_nil!
+      case tag
+      when "resource-drived"
+        ev = Bobo::Application::Events::ResourceDrived.from_wire(data["event"].not_nil!.to_s)
+        new(tag, ev)
+      else
+        raise "not known how to parse #{tag}"
+      end
+    end
+  end
+end
